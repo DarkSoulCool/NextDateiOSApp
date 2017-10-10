3332d4b707f2977f08879a33f0663aafc7d9631c
PANDA_N <nattapongpanda@gmail.com>
2017-10-11 06:12:48 +0700

    Update Commemt
    add comment

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // input field
+    // input textfield
     @IBOutlet weak var monthNumber: UITextField!
     @IBOutlet weak var dayNumber: UITextField!
     @IBOutlet weak var yearNumber: UITextField!
     
-    // output
+    // output textview
     @IBOutlet weak var showNextDate: UITextView!
     
     @IBAction func checkNextDate(_ sender: Any) {
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         var tomorrowYear = year
         
         if (month >= 1 && month <= 12) && (day >= 1 && day <= 31) && (year >= 1812 && year <= 2012) {
+            // month has 31 day
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) {
                 if day <= 30 {
                     tomorrowDay += 1
@@ -36,6 +37,7 @@ class ViewController: UIViewController {
                     tomorrowDay = 1
                     printDate(month: tomorrowMonth, day: tomorrowDay, year: tomorrowYear)
                 }
+            // month has 30 day
             } else if (month == 4 || month == 6 || month == 9 || month == 11) {
                 if day <= 29 {
                     tomorrowDay += 1
@@ -45,18 +47,23 @@ class ViewController: UIViewController {
                     tomorrowDay = 1
                     printDate(month: tomorrowMonth, day: tomorrowDay, year: tomorrowYear)
                 }
+            // check december
             } else if (month == 12) {
                 if day <= 30 {
                     tomorrowDay += 1
                     printDate(month: tomorrowMonth, day: tomorrowDay, year: tomorrowYear)
                 } else {
+                    // new year
                     tomorrowMonth = 1
                     tomorrowDay = 1
                     tomorrowYear += 1
                     printDate(month: tomorrowMonth, day: tomorrowDay, year: tomorrowYear)
                 }
+                // check february
             } else if (month == 2) {
+                // leap year
                 if (year % 4 == 0) {
+                    // 1900 is not leap year
                     if (year == 1900) {
                         if day <= 27 {
                             tomorrowDay += 1
@@ -76,6 +83,7 @@ class ViewController: UIViewController {
                         tomorrowDay = 1
                         printDate(month: tomorrowMonth, day: tomorrowDay, year: tomorrowYear)
                     }
+                // not leap year
                 } else {
                     if day <= 27 {
                         tomorrowDay += 1
@@ -97,6 +105,7 @@ class ViewController: UIViewController {
         
     }
     
+  
     func printDate(month: Int, day: Int, year: Int) {
         showNextDate.text = "\(month), \(day), \(year)"
     }
